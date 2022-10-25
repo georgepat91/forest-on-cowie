31f9f7d92cb656b294c21981a6f9b8063cf13151
georgepat91 <107715486+georgepat91@users.noreply.github.com>
2022-10-25 11:41:36 +0200

    Add files via upload

---
+++ javascript/main.js
@@ -0,0 +1,17 @@
+
+
+const header = document.querySelector("header")
+fetch("header.html")
+.then(res=>res.text())
+.then(data=>{
+    header.innerHTML=data
+})
+
+
+const footer = document.querySelector("footer") 
+fetch("footer.html")
+.then(res=>res.text())
+.then(data=>{
+    footer.innerHTML=data
+})
+
